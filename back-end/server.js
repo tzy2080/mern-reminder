fb9d71f42522ce5fd1c81323faede10cf26f6c28
teohzy2080 <teohzy2080@gmail.com>
2022-11-30 21:48:05 +0800

    Hide database connection string

--- back-end/server.js
+++ back-end/server.js
@@ -23,9 +23,8 @@ app.use('/auth', userRoute);
 
 // Server
 const port = process.env.PORT || 5000;
-const CONNECTION_URL = 'mongodb+srv://test:123@cluster0.v3hcn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 mongoose.set('useFindAndModify', false);
-mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.DATABASE_CONNECTION_STRING, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(port, () => console.log(`Server running on port ${port}`)))
     .catch(error => console.log(error.message));
 
